b2742e195fa3a399dc7c6f75a9a54419bffa8489
tejHackerDEV <tejasimha222@gmail.com>
2023-06-11 13:08:54 +0530

    feat(router): Add support to lookup the registered routes by path.
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -9,14 +9,25 @@ class Router {
 
   Router() : _rootNode = Node(kPathSectionDivider);
 
+  /// Sanitizes the [path] & return the pathSections
+  /// that can be used for processing. So Before processing
+  /// any path that comes to router, we should sanitize it
+  /// first to avoid any issues.
+  Iterable<String> _sanitizePath(String path) {
+    List<String> pathSections = path.split(kPathSectionDivider);
+    for (int i = pathSections.length - 1; i >= 0; --i) {
+      if (pathSections[i].isEmpty) {
+        pathSections.removeAt(i);
+      }
+    }
+    return pathSections;
+  }
+
   /// Register a new [route]
   void register(Route route) {
-    final pathSections = route.path.split(kPathSectionDivider);
+    final pathSections = _sanitizePath(route.path);
     Node currentNode = _rootNode;
     for (final pathSection in pathSections) {
-      if (pathSection.isEmpty) {
-        continue;
-      }
       final nodeToInsert = Node(pathSection);
       currentNode.childNodes ??= [];
       final childNode = currentNode.childNodes!
@@ -31,6 +42,28 @@ class Router {
       }
     }
     currentNode.route = route;
+  }
+
+  /// Lookup for a `route` that has been registered by [path].
+  /// <br>
+  /// If no `route` is registered with [path] returns `null`.
+  Route? lookup(String path) {
+    final pathSections = _sanitizePath(path);
+    Node? currentNode = _rootNode;
+    if (pathSections.isNotEmpty) {
+      // only check for pathSections if its not empty
+      for (final pathSection in pathSections) {
+        currentNode = currentNode?.childNodes?.firstWhereOrNull(
+          (childNode) => childNode.section == pathSection,
+        );
+        // as there is no node with the pathSection we are looking,
+        // simply break the loop without going further
+        if (currentNode == null) {
+          break;
+        }
+      }
+    }
+    return currentNode?.route;
   }
 
   /// Return all the child [Route]'s of a [node]
--- test/router_test.dart
+++ test/router_test.dart
@@ -2,19 +2,34 @@ import 'package:samba_server/samba_server.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Route tests', () {
+  group('Router tests', () {
     final router = Router();
+    final routesToRegister = [
+      Route('/users', () {}),
+      Route('/users/userId', () {}),
+      Route('/profiles', () {}),
+    ];
 
-    test('Should able to register routes', () {
-      final routesToRegister = [
-        Route('/users', () {}),
-        Route('/users/userId', () {}),
-        Route('/profiles', () {}),
-      ];
+    setUp(() {
       for (final route in routesToRegister) {
         router.register(route);
       }
+    });
+
+    test('Should able to register routes', () {
       expect(router.routes, routesToRegister);
+    });
+
+    test('Should able to lookup routes by path', () {
+      expect(router.lookup(routesToRegister[0].path), routesToRegister[0]);
+      expect(router.lookup(routesToRegister[1].path), routesToRegister[1]);
+      expect(router.lookup(routesToRegister[2].path), routesToRegister[2]);
+    });
+
+    test('Should not be able to lookup routes by path', () {
+      expect(router.lookup('random'), isNull);
+      expect(router.lookup('/random'), isNull);
+      expect(router.lookup('/random/random'), isNull);
     });
   });
 }
