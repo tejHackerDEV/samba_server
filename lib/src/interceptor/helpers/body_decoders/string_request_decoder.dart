0af03f94e89014877857b0b924188c71f9d43222
tejHackerDEV <tejasimha222@gmail.com>
2023-07-23 15:46:42 +0530

    refactor(decoder): Directly pass the content-type as `text/` in the constructor.
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- lib/src/interceptor/helpers/body_decoders/string_request_decoder.dart
+++ lib/src/interceptor/helpers/body_decoders/string_request_decoder.dart
@@ -3,26 +3,20 @@ import 'dart:convert';
 import 'dart:io' as io;
 import 'dart:typed_data';
 
-import '../../../utils/content_types.dart';
 import 'request_decoder.dart';
 
 class StringRequestDecoder extends RequestDecoder<String> {
-  /// An interceptor to decode body of a request which contains `content-type`
-  /// as [ContentTypes.kPlainText] in request headers.
+  /// An interceptor to decode body of a request whose `content-type`
+  /// starts with `text/` in request headers.
   ///
   /// In order to decode the body correctly, one should pass correct
   /// [encoding] strategy used in encoding the request body.
   const StringRequestDecoder({
     Encoding fallbackEncoding = utf8,
   }) : super(
-          contentType: ContentTypes.kPlainText,
+          contentType: 'text/',
           fallbackEncoding: fallbackEncoding,
         );
-
-  @override
-  bool canDecode(io.ContentType contentType) => contentType.value.startsWith(
-        'text/',
-      );
 
   @override
   FutureOr<String> decode(
