7d7cb3a6a7f1b3bf2f0113d7ca75557030ea0d07
tejHackerDEV <tejasimha222@gmail.com>
2023-07-24 22:22:56 +0530

    chore(example): Add an quick start example file
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- example/samba_server_example.dart
+++ example/samba_server_example.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'dart:async';
+
+import 'package:samba_server/samba_server.dart';
+
+class ChatSocketRoute extends WebSocketRoute {
+  @override
+  FutureOr<void> onConnected(WebSocket webSocket) {
+    throw UnimplementedError();
+  }
+}
+
+class HelloRoute extends Route {
+  HelloRoute() : super(HttpMethod.get, '/');
+
+  @override
+  FutureOr<Response> handler(Request request) {
+    return Response.ok(body: 'Hello from SAMBA_SERVER');
+  }
+}
+
+Future<void> main() async {
+  final httpServer = HttpServer();
+  httpServer
+    ..registerRoute(HelloRoute())
+    ..registerRoute(ChatSocketRoute());
+  await httpServer.bind(address: '127.0.0.1', port: 8080);
+}
